78ea696430019a6e3f26b0e1f619f94584ef238a
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-08-19 14:10:52 -0500

    Built out posttagprovider

--- src/components/posttags/PostTagsProvider.js
+++ src/components/posttags/PostTagsProvider.js
@@ -25,12 +25,46 @@ export const PostTagsProvider = (props) => {
       .then(setPostTags);
   };
 
+  const editPostTag = (postTag) => {
+    return fetch(`http://localhost:8000/postTags/${postTag.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("rare_user_id")}`,
+      },
+      body: JSON.stringify(postTag),
+    });
+  };
+
+  const deletePostTag = (postTagId) => {
+    return fetch(`http://localhost:8000/postTags/${postTagId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("rare_user_id")}`,
+      },
+    }).then(getAllPostTags);
+  };
+
+  const createPostTag = (newPostTagObj) => {
+    return fetch(`http://localhost:8000/postTags`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${localStorage.getItem("rare_user_id")}`,
+      },
+      body: JSON.stringify(newPostTagObj),
+    }).then(getAllPostTags);
+  };
+
   return (
     <PostTagsContext.Provider
       value={{
         getTagsByPostId,
         postTags,
         getAllPostTags,
+        editPostTag,
+        deletePostTag,
+        createPostTag,
       }}
     >
       {props.children}
