e6d111555670fd4f869ec8c1b501491c69bd17d3
hann <hmweeks93@gmail.com>
2021-08-19 15:09:30 -0500

    delete button on all post and my post view fully functional, only displaying for owner

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -21,54 +21,59 @@ export const PostList = () => {
 
     const handlePostClick = (id) => {
         getPostById(id)
-        .then(() => history.push(`/posts/${id}`))
+            .then(() => history.push(`/posts/${id}`))
     }
 
-    const handleDeleteModal= (postId) => {
+    const handleDeleteModal = (postId) => {
         Swal.fire({
-        title: "Are you sure?",
-        text: "You will not be able to undo!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Yes, delete it!",
-        cancelButtonText: "Ah, cancel"
+            title: "Are you sure?",
+            text: "You will not be able to undo!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "Ah, cancel"
         }).then((result) => {
-        if (result.isConfirmed) {
-            deletePost(postId).then(() => {
-            Swal.fire(
-                "Deleted!",
-                "Your post has been deleted.",
-                "Success!"
-            ).then(getPostById(parseInt(postId)))
-            })
-        }; 
-    })
+            if (result.isConfirmed) {
+                deletePost(postId).then(() => {
+                    Swal.fire(
+                        "Deleted!",
+                        "Your post has been deleted.",
+                        "Success!"
+                    ).then(getAllPosts)
+                })
+            };
+        })
     };
+
     return (
-        <>
+        <section>
             <h1 className="post-title">All Posts</h1>
 
-            {
-                sortedPosts.map(post => {
-                    return (
-                        <>
+            {sortedPosts.map(post => {
+                return (
+                    <>
                         <div className="post-list">
                             <div>
-                                <div>Title: <Link className="title_link" onClick={() => {handlePostClick(post.id)}}>{post.title}</Link></div>
+                                <div>Title: <Link className="title_link" onClick={() => { handlePostClick(post.id) }}>{post.title}</Link></div>
                                 <div>Author: {post.user.full_name} </div>
                                 <div>Category: {post.category.label}</div>
                             </div>
                         </div>
-<div>
-<button onClick={() => 
-                    {handleDeleteModal(post.id)}} className="button is-rounded remove">Delete
-                    <img className="trashIconPic" src="https://img.icons8.com/material/24/000000/trash--v1.png"/></button>
-                    
-</div>
-                        </>
-                    )
-                })
-            }
-        </>
+                        {post.owner ? (
+                            <div>
+                                <button onClick={() => { handleDeleteModal(post.id) }} className="button is-rounded remove">Delete
+                                    <img className="trashIconPic" src="https://img.icons8.com/material/24/000000/trash--v1.png" /></button>
+
+                            </div>) : (
+                            <></>
+                        )}
+                    </>
+                );
+
+            })}
+
+
+
+        </section>
     )
 }
