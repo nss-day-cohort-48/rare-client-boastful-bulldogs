3d5a7813e140189f36889144d1ff8658849afa3e
Ashton Monzi <anmonzi@gmail.com>
2021-08-20 11:25:41 -0500

    ground work for user profile detail page

---
+++ src/components/user/UserProfileDetail.js
@@ -0,0 +1,26 @@
+import React, { useState, useContext, useEffect } from 'react'
+
+
+
+export const ProfileDetail = () => {
+
+
+
+    return (
+        <>
+            <section>
+                <div>
+                    <h2>{} Profile</h2>
+                </div>
+                <div>
+                    <div>Full Name: {}</div>
+                    <div>Image:</div>
+                    <div>Display Name: {}</div>
+                    <div>Email: {}</div>
+                    <div>Profile Created On: {}</div>
+                    <div>Profile Type: {}</div>
+                </div>
+            </section>
+        </>
+    )
+}
